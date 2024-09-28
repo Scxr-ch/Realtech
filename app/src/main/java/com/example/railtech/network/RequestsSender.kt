bd18228e010c6cccb0e5d4274038dc2525abcef3
AloneTotally <alonzothenoob@gmail.com>
2024-09-28 14:47:11 +0800

    Added StepsInfoObject

--- app/src/main/java/com/example/railtech/network/RequestsSender.kt
+++ app/src/main/java/com/example/railtech/network/RequestsSender.kt
@@ -1,5 +1,6 @@
 package com.example.railtech.network
 
+import com.example.railtech.models.StepsInfoObject
 import com.example.railtech.models.WifiScanObject
 
 suspend fun sendWifiScanData(wifiScanObject: WifiScanObject) {
@@ -20,3 +21,21 @@ suspend fun sendWifiScanData(wifiScanObject: WifiScanObject) {
 
 }
 
+
+suspend fun sendStepsData(stepsInfoObject: StepsInfoObject) {
+
+    // Convert the WifiScanObject to a JSON string
+//    val requestBody = Gson().toJson(wifiScanObject)
+    try {
+        val response = RailtechApi.retrofitService.sendStepsData(stepsInfoObject)
+
+        if (response.isSuccessful) {
+            println("POST request successful: ${response.code()}")
+        } else {
+            println("Error: ${response.code()}")
+        }
+    } catch (e: Exception) {
+        println("POST request failed: ${e.message}")
+    }
+
+}
